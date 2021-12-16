a831446372c3971361cf6717c9404758ba66523a
Josué Fonseca <josuefonseca@unigranrio.br>
2021-12-16 08:17:34 -0300

    ok

---
+++ Inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32 ; //ponto flutuante
+console.log(typeof salario); 
+/*javaScript não faz diferenciação entre numeros inteiros
+ e numeros que tem casas decimais */
+
+ let estaChovendo = true // ou false
+ console.log(typeof estaChovendo);
+ console.log(typeof "teste");
